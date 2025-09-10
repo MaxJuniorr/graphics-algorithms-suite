fbbc6e8e44424540671e69f4869429363f9f8fbc
maxjuniorr <maxpantojaa@gmail.com>
2025-09-10 16:13:24 -0300

    config: 200 default

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from interface.app import Aplicacao
 if __name__ == "__main__":
     print("Iniciando a aplicação de Computação Gráfica...")
     # Define a resolução inicial da grade
-    app = Aplicacao(largura_grid_inicial=80, altura_grid_inicial=80)
+    app = Aplicacao(largura_grid_inicial=200, altura_grid_inicial=200)
     app.executar()
 
